3ac52c9f570569282e04b54c5db496392460b2be
大野　佳樹 <yoshiki@oonoyoshikinoMacBook-Air.local>
2025-06-09 16:57:49 +0900

    staging ブランチ初期コミット

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,7 +14,13 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,::1,api.rbplus-rank-manager.site,rbplus-rank-manager.site')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 
+    'localhost,127.0.0.1,::1,
+    api.rbplus-rank-manager.site,
+    rbplus-rank-manager.site,
+    score-manager-frontend-stg.vercel.app,
+    https://score-manager-backend-stg.onrender.com',
+)),
 
 
     /*
