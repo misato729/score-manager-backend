cc2a7603a2081b8efa5153074bef69687950c8ba
大野　佳樹 <yoshiki@oonoyosikinoAir.corega>
2025-06-11 11:23:59 +0900

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return 'Laravel is alive! 🎉';
@@ -11,6 +12,15 @@ Route::get('/', function () {
 Route::middleware(['web', 'auth:sanctum'])->get('/user', function (Request $request) {
     return response()->json($request->user());
 });
+
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    Auth::guard('web')->logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return response()->json(['message' => 'Logged out']);
+});
+
 // web.php の末尾などに追加
 require __DIR__.'/auth.php';
 
