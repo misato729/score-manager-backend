0f02544b8e0ec3c27c3028fd56a1b12669602225
misato729 <valor729@icloud.com>
2025-08-13 10:12:30 +0900

    HTTPSを強制するように修正

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*'; // 全てのプロキシを信頼
+
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB
+        | Request::HEADER_X_FORWARDED_FOR
+        | Request::HEADER_X_FORWARDED_HOST
+        | Request::HEADER_X_FORWARDED_PORT
+        | Request::HEADER_X_FORWARDED_PROTO;
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'only10'   => \App\Http\Middleware\RestrictToUserIdTen::class,
+            'trust.proxies' => \App\Http\Middleware\TrustProxies::class,
         ]);
 
         //
